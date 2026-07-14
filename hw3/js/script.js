8656d888639ae7ce33db795bb36b2e1fc0fcd258
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2026-07-14 03:46:42 -0700

    hw3 TOGGLE DONE DSFHN

--- hw3/js/script.js
+++ hw3/js/script.js
@@ -1,15 +1,23 @@
 const API_KEY = "fe35034c1e10d84ff51e380e97dbc56e";
 const EMPTY_SEARCH_ERROR = "hi please enter a city";
 const CITY_404_ERROR = "this city doesn't exist :( check your spelling?";
+let temp_unit = 'F';
+// in kelvin
+let temp_min = '273.15';
+let temp_max = '333';
+let temp_now = '300';
+
 
 // search bar...
 document.querySelector('#search-bar').addEventListener("keydown", (e) => {
-    if(e.key === "Enter") {
+    if (e.key === "Enter") {
         console.log("weather search initiated");
         getWeather();
     }
 });
 document.querySelector('#search-icon').addEventListener("click", getWeather);
+document.querySelector('#toggle-btn').addEventListener("click", toggleUnits);
+updateTemps();
 
 
 async function getWeather() {
@@ -30,25 +38,73 @@ async function getWeather() {
         if (data.message == 'city not found') {
             displayError(CITY_404_ERROR);
         } else {
+            // update internal global vars
+            temp_min = data.main.temp_min;
+            temp_max = data.main.temp_max;
+            temp_now = data.main.temp;
             displayWeather(data);
         }
     }
 }
 
+function updateTemps() {
+    document.getElementById('temperature').textContent = `${kelvin_conv(temp_now)}`;
+    document.getElementById('degree').textContent = `°${temp_unit}`; // middle f/c
+    document.getElementById('temp-min').textContent = `${kelvin_conv(temp_min)}°${temp_unit}`;
+    document.getElementById('temp-max').textContent = `${kelvin_conv(temp_max)}°${temp_unit}`;
+}
+
+function toggleUnits() {
+    // toggled
+    // change the var
+    if (temp_unit == 'F') {
+        temp_unit = 'C'
+    } else {
+        temp_unit = 'F';
+    }
+
+    // update existing info
+   
+    updateTemps();
+}
+
 function displayWeather(data) {
     console.log("search successful, displayWeather()..");
-    document.getElementById('temperature') = `${K_to_F_rounded(data.main.temp)}°F`;
-    document.getElementById('temp-min').textContent = `${K_to_F_rounded(data.main.temp_min)}°F`;
-    document.getElementById('temp-max').textContent = `${K_to_F_rounded(data.main.temp_max)}°F`;
+
+    updateTemps();
     document.getElementById('w-main').textContent = data.weather[0].main.toLowerCase();
-    document.getElementById('w-desc') = data.weather[0].description;
-    document.getElementById('advice') = getAdvice(data.weather[0].id);
+    document.getElementById('w-desc').textContent = data.weather[0].description;
+    document.getElementById('advice').textContent = getAdvice(data.weather[0].id);
 }
 
+// converts kelvin (raw temp) to F or C (depending on temp_units)
+function kelvin_conv(K) {
+    switch (temp_unit) {
+        case 'F':
+            return Math.round((K - 273.15) * (9 / 5) + 32);
+            break;
+        case 'C':
+            return Math.round(K - 273.15);
+            break;
+        default:
+            return '-';
+    }
+}
+
+
+// -- end of temperature functions
 // returns advice :3
 function getAdvice(weather_id) {
+    return "bring an umbrella";
+}
+
+function changeBg(weather_id) {
 
 }
+
+
+
+
 
 //todo change background
 
@@ -69,9 +125,8 @@ function clearError() {
     error.style.marginTop = "0";
 }
 
-function K_to_F_rounded (K) {
-    return Math.round(((K - 273.15) * (9/5)) + 32);
-}
+
+
 
 
 
